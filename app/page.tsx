d65ef4ee425253375ae665178dbed0c1c7be4ae2
Zack Hanni <zack.hannI@gmail.com>
2024-04-18 15:19:54 -0400

    hide content for now

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,8 @@ export default function Home() {
 
   return (
     <main className="flex justify-center items-center h-screen w-full">
-      <ProfileCard currentAnimal={dogAshWoodinPlain} />
+      {/* <ProfileCard currentAnimal={dogAshWoodinPlain} /> */}
+      hello there
     </main>
   );
 }
