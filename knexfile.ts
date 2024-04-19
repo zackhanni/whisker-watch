87affc85e97cc17a67f9482b00976cd25b3a9892
Zack Hanni <zack.hannI@gmail.com>
2024-04-19 17:19:22 -0400

    Create knexfile.ts

---
+++ knexfile.ts
@@ -0,0 +1,59 @@
+import { Knex } from "knex";
+
+const config: Knex.Config = {
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    extension: "ts",
+  },
+};
+
+export default config;
+
+// import type { Knex } from "knex";
+
+// // Update with your config settings.
+
+// const config: { [key: string]: Knex.Config } = {
+//   development: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: "./dev.sqlite3"
+//     }
+//   },
+
+//   staging: {
+//     client: "postgresql",
+//     connection: {
+//       database: "my_db",
+//       user: "username",
+//       password: "password"
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: "knex_migrations"
+//     }
+//   },
+
+//   production: {
+//     client: "postgresql",
+//     connection: {
+//       database: "my_db",
+//       user: "username",
+//       password: "password"
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: "knex_migrations"
+//     }
+//   }
+
+// };
+
+// module.exports = config;
